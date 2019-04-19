5b1bb41f5bfda3cb8e53e8d36bcc4ec3fb0ea55e
Rayyan Hussain <rayyanhussain220@gmail.com>
2019-04-19 12:40:11 -0700

    edited front page

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/board';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-            Learn React
+      
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">QuiccBoard</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Board />
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ kanban-app/src/components/board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Board extends Component{
+    render(){
+        return (
+        <div>
+            <br/>
+            <div className = "">Board goes here</div>
+        </div>
+        
+        )
+    }
+}
